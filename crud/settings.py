5e9c83c8e1f03070b3f2b2905724657ccab3c83f
anhgodslayer <111446011+anhgodslayer@users.noreply.github.com>
2023-03-04 11:17:52 +0700

    Update settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
          'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'djangodb',  
         'USER':'root',  
-        'PASSWORD':'Quocanh0972108427',  
+        'PASSWORD':'Yourpassword',  
         'HOST':'localhost',  
         'PORT':'3306'  
     }
