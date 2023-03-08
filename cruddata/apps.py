0942cd65cc5bf4412b70d938c3cb34d3d00b6f87
anhgodslayer <111446011+anhgodslayer@users.noreply.github.com>
2023-03-08 13:05:08 +0700

    Delete cruddata directory

--- cruddata/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CruddataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cruddata'
--- cruddata/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms  
-from cruddata.models import Crud 
-class CrudForm(forms.ModelForm):  
-    class Meta:  
-        model = Crud  
-        fields = "__all__"  
